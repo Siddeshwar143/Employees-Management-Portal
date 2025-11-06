b486d2d7bb6735b5312901ab58dde2a405992581
Siddeshwar <siddeshwarmalige5@gmail.com>
2025-11-06 18:42:59 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCDHProject.Models
+{
+    public class Customer
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Custid { get; set; }
+        
+        [MaxLength(50)]
+        [Column(TypeName ="Varchar")]
+        public string? Name { get; set; }
+
+        [Column(TypeName ="Money")]
+        public decimal? Balance { get; set; }
+
+        [MaxLength(50)]
+        [Column(TypeName = "Varchar")]
+        public string? City { get; set; }
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? photo { get; set; }
+        public bool Status { get; set; }
+    }
+}
