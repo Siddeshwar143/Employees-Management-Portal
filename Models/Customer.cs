b40a2747561cb3b4c1a9bc1d8b99fddde9485459
Siddeshwar <siddeshwarmalige5@gmail.com>
2025-11-06 18:42:59 +0530

    Add files via upload

---
+++ Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MVCDHProject.Models
+***REMOVED***
+    public class Customer
+    ***REMOVED***
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Custid ***REMOVED*** get; set; ***REMOVED***
+        
+        [MaxLength(50)]
+        [Column(TypeName ="Varchar")]
+        public string? Name ***REMOVED*** get; set; ***REMOVED***
+
+        [Column(TypeName ="Money")]
+        public decimal? Balance ***REMOVED*** get; set; ***REMOVED***
+
+        [MaxLength(50)]
+        [Column(TypeName = "Varchar")]
+        public string? City ***REMOVED*** get; set; ***REMOVED***
+
+        [Column(TypeName = "nvarchar(max)")]
+        public string? photo ***REMOVED*** get; set; ***REMOVED***
+        public bool Status ***REMOVED*** get; set; ***REMOVED***
+  ***REMOVED***
+***REMOVED***
---
+++ Models/UserViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCDHProject.Models
+***REMOVED***
+    [Keyless]
+    public class UserViewModel
+    ***REMOVED***
+        [Required]
+        public string? Name ***REMOVED*** get; set; ***REMOVED***
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string? Password ***REMOVED*** get; set; ***REMOVED***
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")]
+        [Compare("Password",ErrorMessage ="Confirm Password Should match with Password.")]
+        public string? ConfromPassword ***REMOVED*** get; set; ***REMOVED***
+
+        [Required]
+        [EmailAddress]
+        [Display(Name ="Email Id")]
+        public string? Email ***REMOVED*** get; set; ***REMOVED***
+
+        [Required]
+        [RegularExpression("[6-9]\\d***REMOVED***9***REMOVED***",ErrorMessage ="Moblie No. is Invalid")]
+        public string? Mobile ***REMOVED*** get; set; ***REMOVED***
+  ***REMOVED***
+***REMOVED***
