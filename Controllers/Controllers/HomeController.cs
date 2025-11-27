11b163e955027d80d024258fa22ab146c2a8f09e
Siddeshwar <siddeshwarmalige5@gmail.com>
2025-11-27 18:15:20 +0530

    Add files via upload

---
+++ Controllers/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVCDHProject.Models;
+
+namespace MVCDHProject.Controllers
+***REMOVED***
+    public class HomeController : Controller
+    ***REMOVED***
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        ***REMOVED***
+            _logger = logger;
+      ***REMOVED***
+
+        public IActionResult Index()
+        ***REMOVED***
+            return View();
+      ***REMOVED***
+
+        public IActionResult Privacy()
+        ***REMOVED***
+            return View();
+      ***REMOVED***
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        ***REMOVED***
+            return View(new ErrorViewModel ***REMOVED*** RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier ***REMOVED***);
+      ***REMOVED***
+  ***REMOVED***
+***REMOVED***
